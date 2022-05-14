13d0bd517e760a176203f0ce404f843cd7d18c4c
Кирилл Зикевский <2309696@mail.ru>
2022-05-14 17:11:18 +0300

    dev: dev in progress

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,32 @@
 
+import logo from '../../images/logo.svg';
+import Navigation from '../Navigation/Navigation';
+import { Link, useLocation } from 'react-router-dom';
+import './Header.css';
+
 function Header (props) {
 
-  return (
-    <header className="">
+  let location = useLocation();
 
+  return (
+    <header className="header">
+      <Link className="header-logo__link" to="/" target="_self">
+        <img className="header-logo__img" src={logo} alt="Логотип: буква С в кружочке"/>
+      </Link>
+      {
+        location.pathname === "/" ?
+        (
+          <div>
+            <Link className="header__signup-link" to="/sign-up">Регистрация</Link>
+            <Link className="header__signin-link" to="/sign-in">Войти</Link>
+          </div>
+
+        )
+        :
+        (
+          <Navigation />
+        )
+      }
     </header>
   );
 }
