74dce579bbf4f1699cc586673539f6bdd9c0c7d6
Кирилл Зикевский <2309696@mail.ru>
2022-05-21 19:05:28 +0300

    dev: dev in progress

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import './Main.css';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 function Main (props) {
 
@@ -8,6 +9,7 @@ function Main (props) {
     <main className="main">
       <Promo />
       <AboutProject />
+      <Techs />
     </main>
   );
 }
---
+++ src/components/Techs/Techs.js
@@ -0,0 +1,27 @@
+import './Techs.css';
+
+function Techs () {
+
+  return (
+    <section className="techs">
+      <h2 className="techs__header">Технологии</h2>
+      <article className="techs__article">
+        <h3 className="techs__article-title">7 технологий</h3>
+        <p className="techs__article-text">На курсе веб-разработки мы освоили технологии, которые применили
+          в дипломном проекте.
+        </p>
+      </article>
+      <ul className="techs__list">
+        <li className="techs__list-item">HTML</li>
+        <li className="techs__list-item">CSS</li>
+        <li className="techs__list-item">JS</li>
+        <li className="techs__list-item">React</li>
+        <li className="techs__list-item">Git</li>
+        <li className="techs__list-item">Express.js</li>
+        <li className="techs__list-item">MongoDB</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Techs;
