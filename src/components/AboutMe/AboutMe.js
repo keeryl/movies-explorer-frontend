cd12b472feb388f25b04e3cca6ed104ef30b0535
Кирилл Зикевский <2309696@mail.ru>
2022-05-27 19:17:40 +0300

    dev: dev in progress

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,14 +8,16 @@ function AboutMe () {
       <div className="about-me__wrapper">
         <h2 className="about-me__title">Студент</h2>
         <article className="about-me__article">
-          <div className="about-me__text-wrapper">
-            <h3 className="about-me__name">Зикевский Кирилл</h3>
-            <h4 className="about-me__description">Фронтенд-разработчик, 32 года</h4>
-            <p className="about-me__intro">
-              Я живу в Москве, закончил юридический факультет РУДН.
-              Я люблю слушать музыку, в свободное время хожу в спорт-зал.
-              Недавно начал кодить. После того, как прошёл курс по веб-разработке, начал поиск работы.
-            </p>
+          <div className="about-me__text-column">
+            <div className="about-me__text">
+              <h3 className="about-me__name">Зикевский Кирилл</h3>
+              <h4 className="about-me__description">Фронтенд-разработчик, 32 года</h4>
+              <p className="about-me__intro">
+                Я живу в Москве, закончил юридический факультет РУДН.
+                Я люблю слушать музыку, в свободное время хожу в спорт-зал.
+                Недавно начал кодить. После того, как прошёл курс по веб-разработке, начал поиск работы.
+              </p>
+            </div>
             <ul className="about-me__links">
               <li>
                 <a
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { Route, withRouter, Routes, useHistory } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Header from '../Header/Header.js';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
@@ -11,38 +11,11 @@ function App() {
 
   return (
     <div className="App">
+      <Header loggedIn={loggedIn}/>
       <Routes>
-        <Route exact path="/">
-          <Header loggedIn={loggedIn}/>
-          <Main />
-          <Footer />
-        </Route>
-        <Route exact path="/movies">
-          <Header loggedIn={loggedIn}/>
-          {/* <Movies /> */}
-          <Footer />
-        </Route>
-        <Route exact path="/saved-movies">
-          <Header loggedIn={loggedIn}/>
-          {/* <SavedMovies /> */}
-          <Footer />
-        </Route>
-        <Route exact path="/profile">
-          <Header loggedIn={loggedIn}/>
-          {/* <Profile /> */}
-        </Route>
-        <Route exact path="/signin">
-          <Header loggedIn={loggedIn}/>
-          <Footer />
-        </Route>
-        <Route exact path="/signup">
-          <Header loggedIn={loggedIn}/>
-          <Footer />
-        </Route>
-        <Route exact path="/error_404">
-          {/* <Error_404 /> */}
-        </Route>
+        <Route path="/" element={<Main />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
