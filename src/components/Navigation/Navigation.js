f522d29bbc03f2477b406e058f135cf59936fb4e
Кирилл Зикевский <2309696@mail.ru>
2022-06-01 17:55:33 +0300

    dev: dev in progress

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,18 @@
 import './Navigation.css';
 import { Link } from 'react-router-dom';
+import React from 'react';
 
 function Navigation (props) {
+
+  const [isOpened, setIsOpened] = React.useState(false);
+
+  function openMenu () {
+    setIsOpened(true);
+  }
+
+  function closeMenu () {
+    setIsOpened(false);
+  }
 
   return (
     <nav className="navigation">
@@ -13,14 +24,16 @@ function Navigation (props) {
         Аккаунт
         <div className="navigation__icon"></div>
       </Link>
-      <button className="navigation__humburger-btn"></button>
-      <ul className="navigation__side-menu">
-        <button>Х</button>
-        <li>Главная</li>
-        <li>Фильмы</li>
-        <li>Сохранённые фильмы</li>
-        <li>Аккаунт</li>
-      </ul>
+      <button className="navigation__humburger-btn" onClick={openMenu}></button>
+      <div className={`navigation__popup ${isOpened ? "navigation__popup_opened" : ""}`}>
+        <ul className="navigation__side-menu">
+          <button className="navigation__side-menu-close-btn" onClick={closeMenu}>Х</button>
+          <li>Главная</li>
+          <li>Фильмы</li>
+          <li>Сохранённые фильмы</li>
+          <li>Аккаунт</li>
+        </ul>
+      </div>
     </nav>
   )
 }
