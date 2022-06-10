7e56c8886b17d87d2888236fea5c15ca3b270a15
Кирилл Зикевский <2309696@mail.ru>
2022-06-10 20:08:41 +0300

    dev: dev in progress

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,6 +1,6 @@
 import './MoviesCard.css';
 
-function MoviesCard () {
+function MoviesCard (props) {
 
   const handleCardClick = () => {
 
@@ -14,11 +14,11 @@ function MoviesCard () {
     <li onClick={handleCardClick} className="movies-card">
       <img
         className="movies-card__image"
-        src="https://api.nomoreparties.co/uploads/medium_posters_came_from_the_walls_2009_001_posters_180fe1a19f.jpeg"
+        src={props.url}
         alt={`Картинка фильма «Роллинг Стоунз» в изгнании`}
       />
       <div className="movies-card__description-container">
-        <h2 className="movies-card__title">«Роллинг Стоунз» в изгнании</h2>
+        <h2 className="movies-card__title">{props.title}</h2>
         <button onClick={handleLikeClick} className="movies-card__like-button" type="button"></button>
       </div>
       <span className="movies-card__duration">1ч 42м</span>
