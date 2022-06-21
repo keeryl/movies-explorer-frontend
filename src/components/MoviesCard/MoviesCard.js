48c6bf0b861a441281f6cd47d7ccd5f0a0047deb
Кирилл Зикевский <2309696@mail.ru>
2022-06-21 20:42:21 +0300

    dev: dev in progress

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -12,11 +12,13 @@ function MoviesCard (props) {
 
   return(
     <li onClick={handleCardClick} className="movies-card">
-      <img
-        className="movies-card__image"
-        src={props.url}
-        alt={`Картинка фильма «Роллинг Стоунз» в изгнании`}
-      />
+      <div className="movies-card__image-container">
+        <img
+          className="movies-card__image"
+          src={props.url}
+          alt={`Картинка фильма «Роллинг Стоунз» в изгнании`}
+        />
+      </div>
       <div className="movies-card__description-container">
         <h2 className="movies-card__title">{props.title}</h2>
         <button onClick={handleLikeClick} className="movies-card__like-button" type="button"></button>
