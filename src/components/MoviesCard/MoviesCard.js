65cacc54caab36b57929206fbe38d9988455ade0
Кирилл Зикевский <2309696@mail.ru>
2022-06-25 16:48:28 +0300

    dev: dev in progress

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,13 +1,19 @@
 import './MoviesCard.css';
+import React from 'react';
 
 function MoviesCard (props) {
+
+  const [isLiked, setIsLiked] = React.useState(false);
+  const cardLikeBtnClassName = (
+    `movies-card__like-button ${ isLiked && `movies-card__like-button_liked`}`
+  );
 
   const handleCardClick = () => {
 
   }
 
   const handleLikeClick = () => {
-
+    setIsLiked(!isLiked);
   }
 
   return(
@@ -21,7 +27,11 @@ function MoviesCard (props) {
       </div>
       <div className="movies-card__description-container">
         <h2 className="movies-card__title">{props.title}</h2>
-        <button onClick={handleLikeClick} className="movies-card__like-button" type="button"></button>
+        <button
+          onClick={handleLikeClick}
+          className={cardLikeBtnClassName}
+          type="button">
+        </button>
       </div>
       <span className="movies-card__duration">1ч 42м</span>
     </li>
